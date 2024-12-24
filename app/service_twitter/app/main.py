b221c45036dde64a052cead4382a9edf7fe8d925
Vasily Ryabtsev <vasilyabc@gmail.com>
2024-12-24 20:06:01 +0300

    modify

--- app/service_twitter/app/main.py
+++ app/service_twitter/app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import pickle
+import pandas as pd
+from FinBert import finbert, preprocessing
+import transformers
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer
+import transformers
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import numpy as np
+import pandas as pd
 
 app = FastAPI()
 
+with open('model', 'rb') as input_file:
+    model = pickle.load(input_file)
+
 @app.get("/twitter")
 async def server1_endpoint():
-    return {"message": "Hello from twitter!"}
+    ans = model.predict(pd.DataFrame(data={'text': ['BUY $APPL']}))
+    return {"message": ans}
