810d59c2b2710305abf9a90330e1c707f908e5f2
Vasily Ryabtsev <vasilyabc@gmail.com>
2024-12-24 20:56:13 +0300

    add

--- app/service_twitter/app/main.py
+++ app/service_twitter/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import pickle
+import pandas as pd
+from FinBert import preprocessing
 
 app = FastAPI()
 
+with open('model', 'rb') as input_file:
+    model = pickle.load(input_file)
+
 @app.get("/twitter")
 async def server1_endpoint():
-    return {"message": "Hello from twitter!"}
+    ans = model.predict(pd.DataFrame(data={'text': ['BUY $APPL']}))
+    print(ans)
+    return {"message": 'Hello from service_twitter!'} 
