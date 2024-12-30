0f97377a9763fd6cff82ab67e64e2fb112cb7bb6
Vasily Ryabtsev <vasilyabc@gmail.com>
2024-12-30 18:41:53 +0300

    fix code style

--- app/service_10k/app/main.py
+++ app/service_10k/app/main.py
@@ -5,7 +5,8 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 import uvicorn
 
-import entities
+from entities import PredictResponse, ParamsEntity
+
 import service
 import config
 
@@ -35,7 +36,7 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/report_prediction")
-async def predict_test(file: UploadFile = File(...)) -> entities.PredictResponse:
+async def predict_test(file: UploadFile = File(...)) -> PredictResponse:
     """
     Predicting probabilities
     """
@@ -47,12 +48,15 @@ async def predict_test(file: UploadFile = File(...)) -> entities.PredictResponse
         return service.predict_text(report)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
-    
+
 
 @app.get("/get_params")
-async def get_params() -> entities.ParamsEntity:
+async def get_params() -> ParamsEntity:
     return service.get_params()
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, log_config=config.LOGGING_CONFIG, host='0.0.0.0', port=8001)
+    uvicorn.run(app,
+                log_config=config.LOGGING_CONFIG,
+                host='0.0.0.0',
+                port=8001)
