c314906f7be42fac9e2ee04ef6dafc1ddd934c45
Vasily Ryabtsev <vasilyabc@gmail.com>
2024-12-30 16:26:02 +0300

    add logs

--- app/service_twitter/app/main.py
+++ app/service_twitter/app/main.py
@@ -1,17 +1,31 @@
 from fastapi import FastAPI, HTTPException, Request, Response
 from pydantic import BaseModel
+from contextlib import asynccontextmanager
 import pickle
 import pandas as pd
+import uvicorn
+import config
+import logging
 
+logger = logging.getLogger('uvicorn.error')
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Загрузка модели
+    """
+    try:
+        logger.info('Началась загрузка модели')
+        with open("model", "rb") as input_file:
+            model = pickle.load(input_file)
+            app.state.model = model
+        logger.info('Модель загружена')
+        yield
+    except FileNotFoundError:
+        raise RuntimeError("Файл модели 'model' не найден")
+    
 # Инициализация приложения
-app = FastAPI()
-
-# Загрузка модели
-try:
-    with open("model", "rb") as input_file:
-        model = pickle.load(input_file)
-except FileNotFoundError:
-    raise RuntimeError("Файл модели 'model' не найден")
+app = FastAPI(lifespan=lifespan)
 
 # Описание структуры входных данных
 class TextInput(BaseModel):
@@ -29,16 +43,16 @@ async def predict_sentiment(request: Request, input_data: TextInput):
     """
     try:
         # Логирование запроса
-        print(f"Получен запрос от клиента: {request.client}")
+        logger.info(f"Получен запрос от клиента: {request.client}")
 
         # Создаем DataFrame
         df = pd.DataFrame({"text": [input_data.text]})
-        print(f"Создан DataFrame: {df}")
+        logger.info(f"Создан DataFrame: {df}")
 
         # Получение предсказаний и вероятностей
-        prediction = model.predict(df)[0]
-        probabilities = model.predict_proba(df)[0].tolist()
-        print(f"Предсказания модели: {prediction}, вероятности: {probabilities}")
+        prediction = app.state.model.predict(df)[0]
+        probabilities = app.state.model.predict_proba(df)[0].tolist()
+        logger.info(f"Предсказания модели: {prediction}, вероятности: {probabilities}")
 
         # Формируем ответ
         return PredictionResponse(
@@ -46,5 +60,7 @@ async def predict_sentiment(request: Request, input_data: TextInput):
             positive_probability=probabilities[1]
         )
     except Exception as e:
-        print(f"Ошибка в обработке запроса: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Ошибка обработки: {str(e)}")
+    
+if __name__ == '__main__':
+    uvicorn.run(app, log_config=config.LOGGING_CONFIG, host='0.0.0.0', port=8004)
