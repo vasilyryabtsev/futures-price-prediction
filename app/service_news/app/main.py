7cf1f12782d60f0dd6dc462c8b9284958a47a545
Vasily Ryabtsev <vasilyabc@gmail.com>
2024-12-30 20:14:11 +0300

    fix

--- app/service_news/app/main.py
+++ app/service_news/app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/news")
 async def server1_endpoint():
-    return {"message": "Hello from news! The service is currently in progress. It will be availiable as soon as possible."}
+    '''
+    Возвращает ответ от сервера.
+    '''
+    return {"message": "Hello from news!"}
