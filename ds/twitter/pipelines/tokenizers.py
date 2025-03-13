7c29fee3643c8ba9e4c878d12b8f485873214bf8
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-13 11:38:52 +0300

    update

--- ds/twitter/pipelines/tokenizers.py
+++ ds/twitter/pipelines/tokenizers.py
@@ -1,20 +1,16 @@
 import re
 import pandas as pd
+from nltk import download
 from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
-tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+download("stopwords")
 
-def clean_tweet(tweet):
-    """
-    Удаляет ссылки, теги и лишние пробелы из текста твита.
-    """
-    tweet = re.sub(r"http\S+", "", tweet)  
-    tweet = re.sub(r"@\w+", "", tweet)
-    tweet = re.sub(r"\s+", " ", tweet).strip()
-    return tweet
+stop_words = set(stopwords.words('english'))
+tokenizer = TweetTokenizer(reduce_len=True)
+stemmer = PorterStemmer()
 
 def tweet_tokenizer(tweet):
-    tweet = tweet.lower()
-    tokens = tokenizer.tokenize(clean_tweet(tweet))
-    return [token for token in tokens if token not in stopwords.words('english')]
+    tokens = tokenizer.tokenize(tweet)
+    return [stemmer.stem(token) for token in tokens if token not in stop_words]
